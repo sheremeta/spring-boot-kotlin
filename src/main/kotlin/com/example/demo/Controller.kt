d50bd2c427d88e79bb549849dd94af1bd546d42f
ashereme <Anatolii.Sheremeta@epay.ingenico.com>
2020-01-10 15:26:11 +0000

    added kotlinx-coroutines-core

--- src/main/kotlin/com/example/demo/Controller.kt
+++ src/main/kotlin/com/example/demo/Controller.kt
@@ -1,5 +1,8 @@
 package com.example.demo
 
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.async
+import kotlinx.coroutines.runBlocking
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
@@ -15,9 +18,27 @@ class Controller {
             Greeting(counter.incrementAndGet(), when (name) {
                 "" -> """
                     Who are you?
-                    Please specify your name with the name param
+                    Please specify your name with the 'name' param
                 """
                 else -> "hello, $name"
+            })
+
+    @GetMapping("/bot")
+    fun bot(@RequestParam(value = "question", defaultValue = "") question: String) =
+            Greeting(counter.incrementAndGet(), when (question) {
+                "" -> """
+                    Please ask something?
+                    Please specify your question with the 'question' param
+                """
+                else -> {
+                    val job1 = GlobalScope.async { " Hello, " }
+                    val job2 = GlobalScope.async { " Is it question for me? " }
+                    val job3 = GlobalScope.async { " $question " }
+
+                    runBlocking {
+                        job1.await() + job2.await() + job3.await()
+                    }
+                }
             })
 }
 
